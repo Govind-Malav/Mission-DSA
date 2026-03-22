361d4b4721a4bf6c2afafd029b7597865d21b432
Govind <govindmalav2004@gmail.com>
2026-03-22 23:48:20 +0530

    pattern 9 added

---
+++ Patterns/pattern9.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        int start =1;
+        if(i%2==0){
+            start =0;
+        }
+        for (int j=0; j<=i; j++){
+            cout<<start<<" ";
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
