a0ffec2d2ff27cddc3a4ea4e755e91b788574e0d
Govind <govindmalav2004@gmail.com>
2026-03-23 20:54:19 +0530

    Pairs (STL) is done

---
+++ Stl_Pairs/explain_pair.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void explainpair(){
+  pair<int, int> pr1={2,8};
+  cout<<pr1.first<<" "<<pr1.second<<endl;
+
+  pair<int, char> pr2={2,'A'};
+  cout<<pr2.first<<" "<<pr2.second<<endl;
+}
+
+int main(){
+    explainpair();
+    return 0;
+}
