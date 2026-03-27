405b45c0760957a7c069cb05acb527891c361e72
Govind <govindmalav2004@gmail.com>
2026-03-27 15:58:40 +0530

    perfect no is done

--- basic_maths/perfectno.cpp
+++ basic_maths/perfectno.cpp
@@ -1,17 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool perfectno(int n){
-    int original =n;
-    int sum=0;
-        for(int i=1; i<=n/2; i++){
-            if(n%i==0){
-                sum = sum +i;
-            }
+    int original = n;
+    if(n==1)return false;
+    int sum =1;
+    for(int i=2; i*i<=n; i++){
+        if(n%i==0){
+            sum=sum+i;
+            if(i!=n/i)
+            sum = sum + n/i;
         }
+
+    }
     return (original == sum);
 }
+
+
+
 int main(){
     int n;
     cin>>n;
     cout<<perfectno(n)<<endl;
-}
+} 
