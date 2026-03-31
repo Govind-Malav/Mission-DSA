3987ea1f0407c968d80f1df5ad6a85f00c42bd5e
Govind <govindmalav2004@gmail.com>
2026-03-31 17:24:32 +0530

    count of odd digit in array is done

---
+++ basic_arrays/countofodd.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countofodd(int arr[],int n){
+    int count =0;
+    for(int i=0; i<n; i++){
+        if(arr[i]%2!=0){
+            count ++;
+        }
+    }
+    return count;
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];    
+    }
+    cout<<countofodd(arr,n)<<endl;
+}
